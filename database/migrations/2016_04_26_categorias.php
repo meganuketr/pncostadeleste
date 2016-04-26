930e9bbde1cd366244b0129ae1d675993203b923
meganuke <meganuke@meganuke-PC>
2016-04-26 23:15:08 +1000

    fix

--- database/migrations/2016_04_26_categorias.php
+++ database/migrations/2016_04_26_categorias.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateInventoryTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
