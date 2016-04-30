e2913d82c0bf6ba63b8aca06b2df59a5eb4bebc1
meganuketr <meganuke@gmail.com>
2016-04-30 21:37:33 +1000

    added partials

---
+++ resources/views/partials/_flash.blade.php
@@ -0,0 +1,11 @@
+@if (Session::has('flash_message'))
+	<div class="alert alert-success {{ Session::has('flash_message_important') ? 'alert_important' :'' }}" >
+		@if (Session::has('flash_message_important'))
+		<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&time;</button>
+		@endif
+		
+		{{Session::get('flash_message') }}
+	</div>
+	
+@endif
+
