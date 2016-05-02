0509fef20153f2cfd3456ffd10b9aade6fdd92c1
meganuketr <meganuke@gmail.com>
2016-05-02 17:19:45 +1000

    fix

--- resources/views/categorias/index.blade.php
+++ resources/views/categorias/index.blade.php
@@ -24,7 +24,7 @@
 						
 					</tr>
 					@foreach ($categorias as $categoria)
-						<tr data-id="{{ categoria->id }}">
+						<tr data-id="{{ $categoria->id }}">
 							<td>{{ $categoria->id }}</td>
 							<td>{{ $categoria->nombre }}</td>
 							<td>
